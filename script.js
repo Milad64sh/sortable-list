58db75b04888793c7090a70f882322e549500f48
Milad64sh <m.shalikarian@gmail.com>
2022-09-26 09:21:42 -0700

    styling

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const dragableList = document.getElementById('draggable-list');
+const draggableList = document.getElementById('draggable-list');
 const check = document.getElementById('check');
 
 const richestPeople = [
@@ -31,13 +31,13 @@ function createList() {
       const listItem = document.createElement('li');
       listItem.setAttribute('data-index', index);
       listItem.innerHTML = `
-  <span class="number">${index + 1}</span
+  <span class="number">${index + 1}</span>
   <div class="draggable" draggable="true">
   <p class="person-name">${person}</p>
   <i class="fas fa-grip-lines"></i>
   </div>
   `;
       listItems.push(listItem);
-      dragableList.appendChild(listItem);
+      draggableList.appendChild(listItem);
     });
 }
